617bf8c8547c9e1da8e3bcd4106d262d6fc037a2
dmholland <kenoshatrader@gmail.com>
2020-07-16 12:49:31 -0400

    Added more to the User class

--- src/main/java/com/product/app/model/User.java
+++ src/main/java/com/product/app/model/User.java
@@ -1,6 +1,15 @@
 package com.product.app.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
     private String name;
     private String passwrd;
     private String email;
@@ -9,25 +18,44 @@ public class User {
 
     }
 
-    public User(String name, String passwrd,String email){
+    public User(String id, String name, String passwrd,String email){
+        this.id=id;
         this.name=name;
         this.passwrd=passwrd;
         this.email=email;
     }
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", passwrd='" + passwrd + '\'' +
+                ", email=" + email +
+                '}';
+    }
+public String getId(){
+        return id;
+    }
+public void setId(String id){
+        this.id=id;
+}
     public String getPasswrd() {
         return passwrd;
     }
 
     public void setPasswrd(String passwrd) {
+
         this.passwrd = passwrd;
     }
 
     public String getName() {
+
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
         this.name = name;
     }
 
@@ -36,6 +64,7 @@ public class User {
     }
 
     public void setEmail(String email) {
+
         this.email = email;
     }
 
