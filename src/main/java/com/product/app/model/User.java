8a6833ef3f66d1625af0b538b0da7ed11bfa5bcb
dmholland <kenoshatrader@gmail.com>
2020-07-23 21:27:10 -0400

    Trying something, the User class being generified

--- src/main/java/com/product/app/model/User.java
+++ src/main/java/com/product/app/model/User.java
@@ -1,5 +1,7 @@
 package com.product.app.model;
 
+import io.swagger.models.auth.In;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,12 +14,13 @@ import java.util.Set;
 @Table(name = "user")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//All getters and setters are now instance variables
+     InstanceVariable<Long> id;
+     InstanceVariable<String> userName;
+     InstanceVariable<String> email;
+     InstanceVariable<String> password;
+     InstanceVariable<Integer> phone;
 
-    private String username;
-
-    private String password;
 
     @Transient
     private String passwordConfirm;
@@ -25,43 +28,5 @@ public class User {
     @ManyToMany
     private Set<Role> roles;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPasswordConfirm() {
-        return passwordConfirm;
-    }
-
-    public void setPasswordConfirm(String passwordConfirm) {
-        this.passwordConfirm = passwordConfirm;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
 }
--- src/main/java/com/product/app/service/UserService.java
+++ src/main/java/com/product/app/service/UserService.java
@@ -7,4 +7,6 @@ public interface UserService {
     void save(User user);
 
     User findByUsername(String username);
+    String getEmail(String username);
+    String getPassword(String username);
 }
